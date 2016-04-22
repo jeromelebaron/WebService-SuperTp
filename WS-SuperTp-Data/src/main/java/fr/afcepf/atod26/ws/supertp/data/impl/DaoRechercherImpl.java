5ee8ad50be9fd14e59a02f0633131db94228fc77
Jerdopler <lebaronjerome@free.fr>
2016-04-22 15:10:55 +0200

    Avancement du tp

--- WS-SuperTp-Data/src/main/java/fr/afcepf/atod26/ws/supertp/data/impl/DaoRechercherImpl.java
+++ WS-SuperTp-Data/src/main/java/fr/afcepf/atod26/ws/supertp/data/impl/DaoRechercherImpl.java
@@ -2,18 +2,22 @@ package fr.afcepf.atod26.ws.supertp.data.impl;
 
 import java.util.List;
 
-import javax.ejb.EJB;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
 import javax.jws.WebService;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceUnit;
 
 import fr.afcepf.atod26.ws.supertp.data.api.IDaoRecherche;
 import fr.afcepf.atod26.ws.supertp.data.entity.MarqueEntity;
 import fr.afcepf.atod26.ws.supertp.data.entity.ProduitEntity;
 
 @WebService(targetNamespace = "http://ejb.supertp.webservice.atod26.afcepf.fr", endpointInterface = "fr.afcepf.atod26.ws.supertp.data.api.IDaoRecherche", serviceName = "daoRecherche")
+@Remote
+@Stateless
 public class DaoRechercherImpl implements IDaoRecherche {
 
-	@EJB
+	@PersistenceUnit(unitName = "WS-SuperTP-Data")
 	private EntityManager entityManager;
 
 	private static final String REQUETE_FIND_ALL_MARQUE = "FROM MarqueEntity m";
