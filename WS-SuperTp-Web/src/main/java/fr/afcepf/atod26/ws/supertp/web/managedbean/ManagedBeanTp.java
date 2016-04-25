fb28db0079e933ffc57b40c7a38f07af0a61e438
Jerdopler <lebaronjerome@free.fr>
2016-04-25 11:39:48 +0200

    MAJ du front

--- WS-SuperTp-Web/src/main/java/fr/afcepf/atod26/ws/supertp/web/managedbean/ManagedBeanTp.java
+++ WS-SuperTp-Web/src/main/java/fr/afcepf/atod26/ws/supertp/web/managedbean/ManagedBeanTp.java
@@ -1,5 +1,9 @@
 package fr.afcepf.atod26.ws.supertp.web.managedbean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
@@ -8,6 +12,27 @@ public class ManagedBeanTp {
 	private String login;
 
 	private String motDePasse;
+
+	private String token;
+
+	private String marqueSelectionnee;
+
+	private List<String> lesMarques;
+
+	@PostConstruct
+	public void init() {
+		// TODO Charger la liste des marques.
+		lesMarques = new ArrayList<>();
+		lesMarques.add("Apple");
+	}
+
+	public String connexion() {
+		return "";
+	}
+
+	public String afficherLesProduits(final String marque) {
+		return "";
+	}
 
 	public String getLogin() {
 		return login;
@@ -23,6 +48,30 @@ public class ManagedBeanTp {
 
 	public void setMotDePasse(String motDePasse) {
 		this.motDePasse = motDePasse;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getMarqueSelectionnee() {
+		return marqueSelectionnee;
+	}
+
+	public void setMarqueSelectionnee(String marqueSelectionnee) {
+		this.marqueSelectionnee = marqueSelectionnee;
+	}
+
+	public List<String> getLesMarques() {
+		return lesMarques;
+	}
+
+	public void setLesMarques(List<String> lesMarques) {
+		this.lesMarques = lesMarques;
 	}
 
 }
